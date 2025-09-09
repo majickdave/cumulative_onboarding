1f694af05f68413604b85d575e46a1b3eac3e7c8
majickdave <majickdave@gmail.com>
2025-09-09 14:12:08 -0700

    changed file_path

--- python_functions/dashboard_functions.py
+++ python_functions/dashboard_functions.py
@@ -327,7 +327,7 @@ def render_metric_card(data, label: str, value: int, delta_pct: float, period_da
 @st.cache_data
 def load_data():
     
-    file_path = '/Users/davidsamuel/Desktop/mindtree_repo/cumulative_onboarding_dash/data/client_onboarding_dates.csv'
+    file_path = '/Users/davidsamuel/Desktop/mindtree_repo/cumulative_onboarding/data/dates.csv'
     # load data
     df = pd.read_csv(file_path, encoding='utf-8')
 
